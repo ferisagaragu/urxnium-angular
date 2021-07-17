595eaf4ab73c39232388d1ea1000f8717fc7e13d
Fernando Isaías García Aguirre <figarcia@gmail.com>
2021-07-16 22:05:59 -0500

    Second init commit

---
+++ projects/ng-urxnium/src/lib/ng-urxnium.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgUrxniumComponent } from './ng-urxnium.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgUrxniumComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgUrxniumComponent
+  ]
+})
+export class NgUrxniumModule { }
---
+++ projects/ng-urxnium/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-urxnium
+ */
+
+export * from './lib/ng-urxnium.service';
+export * from './lib/ng-urxnium.component';
+export * from './lib/ng-urxnium.module';
